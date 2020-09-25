49bf94dce84e6acce6405fb7625ba453a1c21c4e
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-09-25 12:16:01 +0400

    rs

---
+++ qga/lib.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+mod error;
+mod qapi;
+mod qapi_sys;
+mod qemu;
+mod qemu_sys;
+mod qmp;
+mod translate;
---
+++ qga/qapi.rs
@@ -0,0 +1,87 @@
+#![allow(dead_code)]
+use std::convert::{TryFrom, TryInto};
+use std::{ptr, str};
+
+use crate::translate::*;
+
+use crate::qapi_sys;
+use crate::qemu_sys;
+use crate::translate;
+
+include!(concat!(env!("MESON_BUILD_ROOT"), "/qga/qga-qapi-types.rs"));
+
+impl<'a> ToQemuPtr<'a, *mut qapi_sys::GuestFileWhence> for GuestFileWhence {
+    type Storage = Box<qapi_sys::GuestFileWhence>;
+
+    #[inline]
+    fn to_qemu_none(&'a self) -> Stash<'a, *mut qapi_sys::GuestFileWhence, GuestFileWhence> {
+        let mut w = Box::new(self.into());
+
+        Stash(&mut *w, w)
+    }
+}
+
+impl From<&GuestFileWhence> for qapi_sys::GuestFileWhence {
+    fn from(w: &GuestFileWhence) -> Self {
+        match *w {
+            GuestFileWhence::Name(name) => Self {
+                ty: QType::Qstring,
+                u: qapi_sys::GuestFileWhenceUnion { name },
+            },
+            GuestFileWhence::Value(value) => Self {
+                ty: QType::Qnum,
+                u: qapi_sys::GuestFileWhenceUnion { value },
+            },
+        }
+    }
+}
+
+macro_rules! vec_to_qemu_ptr {
+    ($rs:ident, $sys:ident) => {
+        #[allow(non_camel_case_types)]
+        pub struct $sys(*mut qapi_sys::$sys);
+
+        impl Drop for $sys {
+            fn drop(&mut self) {
+                let mut list = self.0;
+                unsafe {
+                    while !list.is_null() {
+                        let next = (*list).next;
+                        Box::from_raw(list);
+                        list = next;
+                    }
+                }
+            }
+        }
+
+        impl<'a> ToQemuPtr<'a, *mut qapi_sys::$sys> for Vec<$rs> {
+            type Storage = (
+                Option<$sys>,
+                Vec<Stash<'a, <$rs as QemuPtrDefault>::QemuType, $rs>>,
+            );
+
+            #[inline]
+            fn to_qemu_none(&self) -> Stash<*mut qapi_sys::$sys, Self> {
+                let stash_vec: Vec<_> = self.iter().rev().map(ToQemuPtr::to_qemu_none).collect();
+                let mut list: *mut qapi_sys::$sys = ptr::null_mut();
+                for stash in &stash_vec {
+                    let b = Box::new(qapi_sys::$sys {
+                        next: list,
+                        value: Ptr::to(stash.0),
+                    });
+                    list = Box::into_raw(b);
+                }
+                Stash(list, (Some($sys(list)), stash_vec))
+            }
+        }
+    };
+}
+
+// TODO: could probably be templated instead
+vec_to_qemu_ptr!(String, strList);
+vec_to_qemu_ptr!(GuestAgentCommandInfo, GuestAgentCommandInfoList);
+vec_to_qemu_ptr!(GuestFilesystemTrimResult, GuestFilesystemTrimResultList);
+vec_to_qemu_ptr!(GuestIpAddress, GuestIpAddressList);
+vec_to_qemu_ptr!(GuestDiskAddress, GuestDiskAddressList);
+vec_to_qemu_ptr!(GuestLogicalProcessor, GuestLogicalProcessorList);
+vec_to_qemu_ptr!(GuestMemoryBlock, GuestMemoryBlockList);
