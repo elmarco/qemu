486dc5da57130f8348c077b4df4c59556b0dfabe
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-10-09 01:06:41 +0400

    qga: bind slog()
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- qga/lib.rs
+++ qga/lib.rs
@@ -1,3 +1,19 @@
+use std::ffi::CString;
+
 mod qapi;
 mod qapi_sys;
 mod qmp;
+
+pub(crate) fn slog(msg: &str) {
+    if let Ok(cs) = CString::new(msg) {
+        unsafe {
+            sys::slog(CString::new("%s").unwrap().as_ptr(), cs.as_ptr());
+        }
+    }
+}
+
+mod sys {
+    extern "C" {
+        pub fn slog(format: *const libc::c_char, ...);
+    }
+}
