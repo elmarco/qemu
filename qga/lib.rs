282c4c9b38e3b666b0b7d5a32ed731c3ab535c22
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-10-08 20:54:43 +0400

    qga: implement get-host-name in Rust
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- qga/lib.rs
+++ qga/lib.rs
@@ -1,5 +1,7 @@
 use std::ffi::CString;
 
+pub use common::{err, Error, Result};
+
 mod qapi;
 mod qapi_sys;
 mod qmp;
--- qga/qmp/mod.rs
+++ qga/qmp/mod.rs
@@ -50,3 +50,10 @@ extern "C" fn qmp_guest_sync(id: i64, _errp: *mut *mut sys::Error) -> i64 {
 extern "C" fn qmp_guest_ping(_errp: *mut *mut sys::Error) {
     slog("guest-ping called");
 }
+
+mod hostname;
+
+#[no_mangle]
+extern "C" fn qmp_guest_get_host_name(errp: *mut *mut sys::Error) -> *mut qapi_sys::GuestHostName {
+    qmp!(hostname::get(), errp)
+}
